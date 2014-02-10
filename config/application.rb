bf054baf7166a34a686508b38440fc4b29f2c294
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-10 12:16:23 -0800

    Moved application configuration from config/environment.rb.rails2 to config/application.rb with 'rake rails:upgrade:configuration'.

--- config/application.rb
+++ config/application.rb
@@ -2,61 +2,76 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module BBYIDX
   class Application < Rails::Application
+    config.autoload_paths += [config.root.join('lib')]
+    config.encoding = 'utf-8'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # See Rails::Configuration for more options.
 
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    # Skip frameworks you're not going to use. To use Rails without a database
+    # you must remove the Active Record framework.
+    # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+    # config.frameworks -=[:action_mailer]
 
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
+    config.autoload_paths += %W(#{Rails.root}/app/jobs)
+
+    # Only load the plugins named here, in the order given. By default, all plugins
+    # in vendor/plugins are loaded in alphabetical order.
+    # :all can be used as a placeholder for all plugins not explicitly named
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+    # Add additional load paths for your own custom dirs
+    # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Force all environments to use the same logger level
+    # (by default production uses :info, the others :debug)
+    # config.log_level = :info
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Make Time.zone default to the specified zone, and make Active Record store time values
+    # in the database in UTC, and return them converted to the specified local zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
+    config.time_zone = 'UTC'
 
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    # SESSION_KEY and SESSION_SECRET are specified in config/environment_custom.rb
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :password_confirmation]
+    if !SESSION_SECRET && SHORT_SITE_NAME == 'BBYIDX'
+      # Allow development against BBYIDX code base, but require custom session ID if user
+      # is doing a custom deployment of the project.
+      SESSION_SECRET = "bbyidx-demo-#{rand()}-#{rand()}-#{rand()}-#{rand()}-#{rand()}"
+    end
+    config.action_controller.session = {
+      :key    => SESSION_KEY,
+      :secret => SESSION_SECRET
+    }
 
-    # Enable escaping HTML in JSON.
-    config.active_support.escape_html_entities_in_json = true
+    # Use the database for sessions instead of the cookie-based default,
+    # which shouldn't be used to store highly confidential information
+    # (create the session table with "rake db:sessions:create")
+    # config.action_controller.session_store = :active_record_store
 
-    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # Use SQL instead of Active Record's schema dumper when creating the test database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # Activate observers that should always be running
+    # config.active_record.observers = :cacher, :garbage_collector
+    config.active_record.observers = :user_observer, :idea_observer
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
+    # disable forgery proction so that facebook works (we might be able to disble this only for the facebook controller)
+    # config.action_controller.allow_forgery_protection = false
+  end
 
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+  unless SESSION_SECRET
+    abort "\n" +
+          "    You must specify a value for SESSION_SECRET in config/environment_custom.rb in order to start the server.\n" +
+          "    The value should be a string of at least 30 random characeters.\n" +
+          "    Here is a suggested value, generated at random just for you:\n\n" +
+          "      SESSION_SECRET = '#{ActiveSupport::SecureRandom.base64(60)}'\n\n"
   end
 end
