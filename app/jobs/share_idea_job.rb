ea922bc1c7073d34703ff5d7130d8c2a6bfd5126
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-10 11:20:23 -0800

    Now using Rails 3 default logger syntax.

--- app/jobs/share_idea_job.rb
+++ app/jobs/share_idea_job.rb
@@ -1,12 +1,12 @@
 class ShareIdeaJob
-  
+
   attr_accessor :idea_id, :idea_url
-  
+
   def initialize(idea, idea_url)
     @idea_id = idea.id
     @idea_url = idea_url  # need to get it here because we don't have access to the idea_url method
   end
-  
+
   def perform
     Timeout::timeout(60) do
       Idea.transaction do
@@ -14,11 +14,11 @@ class ShareIdeaJob
       end
     end
   end
-  
+
   include ActionView::Helpers::TextHelper # for truncate
-  
+
   def logger
-    RAILS_DEFAULT_LOGGER
+    Rails.logger.info
   end
-  
+
 end
