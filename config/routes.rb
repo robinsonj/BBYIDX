97313acdd9c54dd35bfaa49ecc88345ab3404b91
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-10 09:45:38 -0800

    All routes updated to match Rails 3 syntax.

--- config/routes.rb
+++ config/routes.rb
@@ -30,69 +30,33 @@ BBYIDX::Application.routes.draw do
   resource :user, :map
   resources :comments, :tags, :profiles
 
-  match '/login'                              => 'sessions#new', :as              => :login
-  match '/login/twitter'                      => 'sessions#new', :as              => :twitter_login
-  match '/logout'                             => 'sessions#destroy', :as          => :logout
-  match '/signup'                             => 'users#new', :as                 => :signup
-  match '/ideas/search/*search'               => 'ideas#index', :as               => :idea_search
-  match '/user/send_activation'               => 'users#send_activation', :as     => :send_activation
-  match '/user/activate/:activation_code'     => 'users#activate', :as            => :activate
-  match '/user/password/forgot'               => 'users#forgot_password', :as     => :forgot_password
-  match '/user/password/forgot'               => 'users#send_password_reset', :as => :send_password_reset
-  match '/user/password/new/:activation_code' => 'users#new_password', :as        => :password_reset
-  match '/user/authorize/twitter'             => 'users#authorize_twitter', :as   => :authorize_twitter
-  match '/:model/:id/inappropriate'           => 'inappropriate#flag', :as        => :flag_inappropriate
+  match '/login'                              => 'sessions#new',                :as => :login
+  match '/login/twitter'                      => 'sessions#new',                :as => :twitter_login
+  match '/logout'                             => 'sessions#destroy',            :as => :logout
+  match '/signup'                             => 'users#new',                   :as => :signup
+  match '/ideas/search/*search'               => 'ideas#index',                 :as => :idea_search
+  match '/user/send_activation'               => 'users#send_activation',       :as => :send_activation
+  match '/user/activate/:activation_code'     => 'users#activate',              :as => :activate
+  match '/user/password/forgot'               => 'users#forgot_password',       :as => :forgot_password
+  match '/user/password/forgot'               => 'users#send_password_reset',   :as => :send_password_reset
+  match '/user/password/new/:activation_code' => 'users#new_password',          :as => :password_reset
+  match '/user/authorize/twitter'             => 'users#authorize_twitter',     :as => :authorize_twitter
+  match '/:model/:id/inappropriate'           => 'inappropriate#flag',          :as  => :flag_inappropriate
 
   # Pretty URLS: these must come after more specific routes
-  match '/ideas/:id/:title'     => 'ideas#show', :as    => :idea_pretty
+  match '/ideas/:id/:title'     => 'ideas#show',    :as => :idea_pretty
   match '/profiles/:id/:title'  => 'profiles#show', :as => :profile_pretty
   match '/currents/:id/:title'  => 'currents#show', :as => :current_pretty
 
   # OAuth stuff
-  match '/oauth/test_request'   => 'oauth#test_request', :as  => :test_request
-  match '/oauth/access_token'   => 'oauth#access_token', :as  => :access_token
+  match '/oauth/test_request'   => 'oauth#test_request',  :as => :test_request
+  match '/oauth/access_token'   => 'oauth#access_token',  :as => :access_token
   match '/oauth/request_token'  => 'oauth#request_token', :as => :request_token
-  match '/oauth/authorize'      => 'oauth#authorize', :as     => :authorize
-  match '/oauth'                => 'oauth#index', :as         => :oauth
-
-##########
+  match '/oauth/authorize'      => 'oauth#authorize',     :as => :authorize
+  match '/oauth'                => 'oauth#index',         :as => :oauth
 
   # Admin interface
 
-  # Rails 2
-  # map.namespace :admin do |admin|
-  #   admin.root :controller => 'home', :action => 'show'
-  #   admin.resources :users, :member => { :suspend => :put, :unsuspend => :put, :activate => :put}
-  #   admin.resources :comments
-  #   admin.resources :tags
-  #   admin.resources :ideas
-  #   admin.resources :currents
-  #   admin.resources :client_applications
-  #   admin.resource :chronology
-  #   admin.with_options :path_prefix => 'admin/life_cycles', :controller => 'life_cycles' do |life_cycle|
-  #     life_cycle.life_cycles 'edit',                 :action => 'edit'
-  #     life_cycle.connect     'create',               :action => 'create'       # can't use post for these two b/c InPlaceEdtitor...
-  #     life_cycle.connect     ':id/step/create',      :action => 'create_step'  # ...can't post when htmlResponse is false
-  #     life_cycle.connect     ':id/update/order',     :action => 'reorder',                  :conditions => { :method => :post }
-  #     life_cycle.connect     ':id/update/name',      :action => 'set_life_cycle_name',      :conditions => { :method => :post }
-  #     life_cycle.connect     ':id/delete',           :action => 'delete',                   :conditions => { :method => :delete }
-  #     life_cycle.connect     'step/:id/update/name', :action => 'set_life_cycle_step_name', :conditions => { :method => :post }
-  #     life_cycle.connect     'step/:id/delete',      :action => 'delete_step',              :conditions => { :method => :delete }
-  #   end
-  #   admin.similar_ideas 'ideas/similar/:similar_to', :controller => 'ideas', :action => 'index'
-  #   admin.similar_comments 'comments/similar/:similar_to', :controller => 'comments', :action => 'index'
-  #   admin.with_options :path_prefix => 'admin/bucket', :controller => 'buckets', :name_prefix => 'admin_bucket_' do |bucket|
-  #     bucket.show        '',                :action => 'show'
-  #     bucket.add_idea    'add/:idea_id',    :action => 'add_idea',    :conditions => { :method => :put }
-  #     bucket.remove_idea 'remove/:idea_id', :action => 'remove_idea', :conditions => { :method => :delete }
-  #   end
-  #   admin.with_options :controller => 'ideas' do |dup|
-  #     dup.compare_duplicates 'ideas/:id/link_duplicate/:other_id', :action => 'compare_duplicates', :conditions => { :method => :get }
-  #     dup.link_duplicates    'ideas/:id/link_duplicate/:other_id', :action => 'link_duplicates',    :conditions => { :method => :post }
-  #   end
-  # end
-
-  # Rails 3
   namespace :admin do
     root :to => 'home#show'
     resources :comments, :tags, :ideas, :currents, :client_applications
@@ -106,11 +70,31 @@ BBYIDX::Application.routes.draw do
       end
     end
 
-    match 'ideas/similar/:similar_to' => 'ideas#index', :as => :similar_ideas
-    match 'comments/similar/:similar_to' => 'comments#index', :as => :similar_comments
-  end
+    scope '/life_cycle', :path_prefix => 'admin/life_cycles' do
+      match   'edit'                  => 'life_cycles#edit',                      :as => :life_cycles
+      match   'create'                => 'life_cycles#create',                    :as => :connect # can't use post for these two b/c InPlaceEdtitor...
+      match   ':id/step/create'       => 'life_cycles#create_step',               :as => :connect # ...can't post when htmlResponse is false
+      post    ':id/update/order'      => 'life_cycles#reorder',                   :as => :connect
+      post    ':id/update/name'       => 'life_cycles#set_life_cycle_name',       :as => :connect
+      delete  ':id/delete'            => 'life_cycles#delete',                    :as => :connect
+      post    'step/:id/update/name'  => 'life_cycles#set_life_cycle_step_name',  :as => :connect
+      delete  'step/:id/delete'       => 'life_cycles#delete_step',               :as => :connect
+    end
 
-##########
+    match 'ideas/similar/:similar_to'     => 'ideas#index',     :as => :similar_ideas
+    match 'comments/similar/:similar_to'  => 'comments#index',  :as => :similar_comments
+
+    scope '/bucket',:name_prefix => 'admin_bucket_', :path_prefix => 'admin/bucket' do
+      match   ''                => 'buckets#show',        :as => :show
+      put     'add/:idea_id'    => 'buckets#add_idea',    :as => :add_idea
+      delete  'remove/:idea_id' => 'buckets#remove_idea', :as => :remove_idea
+    end
+
+    scope '/dup' do
+      get   'ideas/:id/link_duplicate/:other_id' => 'ideas#compare_duplicates', :as => :compare_duplicates
+      post  'ideas/:id/link_duplicate/:other_id' => 'ideas#link_duplicates',    :as => :link_duplicates
+    end
+  end
 
   # Top-level routes
   root :to => 'home#show'
