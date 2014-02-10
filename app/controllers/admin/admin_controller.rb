27f8487ccd8aeb768a513fec4795a0c94f623d5e
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-10 11:14:45 -0800

    Updated some of the old ActiveRecord calls. Old format is deprecated.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,18 +3,18 @@
 module Admin
   class AdminController < ApplicationController
     layout 'admin'
-    
+
     before_filter :admin_required
     before_filter :editor_required, :except => [:index, :show, :edit, :new, :create]
     # TODO: If we want create permission separate from edit permission, uncomment the
     # following and remove new & create from previous line:
     #before_filter :creator_required, :only => [:new, :create]
-    
+
     def show_if_nonzero(n)
       (n.to_f != 0) ? n : '&nbsp;'
     end
     helper_method :show_if_nonzero
-  
+
     def paginate_pending_moderation(current_model, default_per_page, sort_sql = 'inappropriate_flags desc')
       current_model.paginate(
         :all,
@@ -23,16 +23,17 @@ module Admin
         :page => params[:page],
         :per_page => params[:per_page] || default_per_page)
     end
-  
+
     def request_as_words
       super + " in the admin interface"
     end
-    
+
     def all_life_cycles
-      @all_life_cycles ||= LifeCycle.find(:all, :include => :steps, :order => 'created_at').reject { |lc| lc.steps.empty? }
+      #@all_life_cycles ||= LifeCycle.find(:all, :include => :steps, :order => 'created_at').reject { |lc| lc.steps.empty? }
+      @all_life_cycles ||= LifeCycle.include('steps').order('created_at').all.reject { |lc| lc.steps.empty? }
     end
     helper_method :all_life_cycles
-    
+
     def disable_form_unless_editor(form_object)
       unless editor?
         "<script type='text/javascript'>
@@ -47,6 +48,6 @@ module Admin
       end
     end
     helper_method :disable_form_unless_editor
-    
+
   end
 end
--- app/models/postal_code.rb
+++ app/models/postal_code.rb
@@ -1,12 +1,13 @@
 class PostalCode < ActiveRecord::Base
-  
+
   def self.find_by_text(text)
     text =~ /\d{5}/
-    PostalCode.find(:first, :conditions => { :code => $& })
+    #PostalCode.find(:first, :conditions => { :code => $& })
+    PostalCode.where(:code => $&).first
   end
-  
+
   def readonly?
     true
   end
-  
+
 end
