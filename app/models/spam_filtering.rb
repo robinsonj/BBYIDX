a28210bfd19fc44fa0c15b645925ea9d731373c8
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-10 16:17:58 -0800

    Updated 'rakismet' gem usage to fit new syntax.

--- app/models/spam_filtering.rb
+++ app/models/spam_filtering.rb
@@ -1,23 +1,24 @@
 module SpamFiltering
   def self.included(klass)
     klass.class_eval do
-      has_rakismet :author       => proc { spam_filtering_user.name },
-                   :author_email => proc { spam_filtering_user.email },
-                   :user_ip    => :ip,
-                   :user_agent => :agent,
-                   :content    => :spam_filtering_text,
-                   :permalink  => proc { "#{Rakismet::URL}/#{self.class.name.pluralize.underscore}/#{self.id}" }
-  
+      include Rakismet::Model
+      rakismet_attrs  :author       => proc { spam_filtering_user.name },
+                      :author_email => proc { spam_filtering_user.email },
+                      :user_ip    => :ip,
+                      :user_agent => :agent,
+                      :content    => :spam_filtering_text,
+                      :permalink  => proc { "#{Rakismet::URL}/#{self.class.name.pluralize.underscore}/#{self.id}" }
+
       validates_presence_of :ip, :on => :create
       validates_presence_of :user_agent, :on => :create
     end
   end
-  
+
   def marked_spam=(spam)
     self[:marked_spam] = spam
     self[:hidden] = true if spam
   end
-  
+
   def check_spam!
     Timeout::timeout(60) do
       self.marked_spam ||= self.spam? if Rakismet::KEY
@@ -25,5 +26,5 @@ module SpamFiltering
       self.save!
     end
   end
-  
+
 end
