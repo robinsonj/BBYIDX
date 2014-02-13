72dba24229275f35c0949b5454af2e4e38c7fed6
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-12 16:17:31 -0800

    Introduced replacement for the deprecated 'error_messages_for' method.
    Replacement can be temporary and allows for deeper customization of how the messages will be rendered out to the user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,6 +72,31 @@ module ApplicationHelper
       'total-count' => p.total_entries }
   end
 
+  # error_messages_for replacement from http://www.emersonlackey.com/article/rails3-error-messages-for-replacement.
+  # Should be changed to reflect preferred stylings.
+  def errors_for(object, message=nil)
+    html = ""
+    unless object.errors.blank?
+      html << "<div class='formErrors #{object.class.name.humanize.downcase}Errors'>\n"
+      if message.blank?
+        if object.new_record?
+          html << "\t\t<h5>There was a problem creating the #{object.class.name.humanize.downcase}</h5>\n"
+        else
+          html << "\t\t<h5>There was a problem updating the #{object.class.name.humanize.downcase}</h5>\n"
+        end
+      else
+        html << "<h5>#{message}</h5>"
+      end
+      html << "\t\t<ul>\n"
+      object.errors.full_messages.each do |error|
+        html << "\t\t\t<li>#{error}</li>\n"
+      end
+      html << "\t\t</ul>\n"
+      html << "\t</div>\n"
+    end
+    html
+  end
+
 private
 
   def flagged_as_inappropriate_session_key(model)
