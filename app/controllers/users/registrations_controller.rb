8fc7e5174c32e2a479f451daae46a1d3bd533038
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-21 17:55:15 -0800

    Some user creation moved here from the old user controller.
    Not all of it is where it should be and still needs to be taken care of.
    Registration isn't completely working (passwords aren't saved, etc).
    Does not know how to properly redirect after registration is complete.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,48 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # def create
-  #   super
-  # end
+  def create
+    if params[:first_user]
+      if User.count > 0  # Critical line! Never set @first_user unless we've verified that this is really the first user.
+        flash[:info] = 'An admin user has already been created.'
+        redirect_to :action => :new
+        return
+      end
+      @first_user = true
+    end
+
+    cookies.delete :auth_token
+    new_user_from_params
+    if @user.valid?
+      @user.save!
+      @user.register!
+      @user.activate! if @user.linked_to_twitter? || @user.linked_to_facebook?
+      promote_to_superuser if @first_user
+      self.current_user = @user
+      flash[:info] = render_to_string(:partial => 'created')
+      #redirect_back_or_default('/')
+      #send_account_state_notification @user
+      super
+    else
+      if @user.linked_to_twitter? || @user.linked_to_facebook?
+        render :action => 'new_via_third_party'
+      else
+        render :action => 'new'
+      end
+    end
+  end
+
+protected
+
+  def new_user_from_params
+    @user = User.new(params[:user])
+    @user.twitter_token = params[:user][:twitter_token]
+    @user.twitter_secret = params[:user][:twitter_secret]
+    @user.twitter_handle = params[:user][:twitter_handle]
+    if FACEBOOK_ENABLED && current_facebook_user
+      @user.facebook_name = params[:user][:facebook_name]
+      @user.facebook_uid = current_facebook_user.id
+      @user.facebook_access_token = current_facebook_client.access_token
+    end
+  end
 end
