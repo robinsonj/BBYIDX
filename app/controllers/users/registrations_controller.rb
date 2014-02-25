58b61609125d6ba2abae27b76c799e1d4abd7d00
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-24 17:08:23 -0800

    Using basic registration controller for the time being.
    No twitter/facebook authentication is being done at the moment. Only authentication checks are being done by devise.
    Also added missing filters for when to require user authentication. Any other actions do not require logging in.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,37 +1,42 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
+  before_filter :authenticate_user!, :only => [:edit, :update, :authorize_twitter]
+
   # def new
   #   super
   # end
 
   def create
-    if params[:first_user]
-      if User.count > 0  # Critical line! Never set @first_user unless we've verified that this is really the first user.
-        flash[:info] = 'An admin user has already been created.'
-        redirect_to :action => :new
-        return
-      end
-      @first_user = true
-    end
+    # if params[:first_user]
+    #   if User.count > 0  # Critical line! Never set @first_user unless we've verified that this is really the first user.
+    #     flash[:info] = 'An admin user has already been created.'
+    #     redirect_to :action => :new
+    #     return
+    #   end
+    #   @first_user = true
+    # end
 
-    cookies.delete :auth_token
-    new_user_from_params
-    if @user.valid?
-      @user.save!
-      @user.register!
-      @user.activate! if @user.linked_to_twitter? || @user.linked_to_facebook?
-      promote_to_superuser if @first_user
-      self.current_user = @user
-      flash[:info] = render_to_string(:partial => 'created')
-      #redirect_back_or_default('/')
-      #send_account_state_notification @user
-      super
-    else
-      if @user.linked_to_twitter? || @user.linked_to_facebook?
-        render :action => 'new_via_third_party'
-      else
-        render :action => 'new'
-      end
-    end
+    # cookies.delete :auth_token
+    # new_user_from_params
+    # if @user.valid?
+    #   @user.save!
+    #   @user.register!
+    #   @user.activate! if @user.linked_to_twitter? || @user.linked_to_facebook?
+    #   promote_to_superuser if @first_user
+    #   self.current_user = @user
+    #   flash[:info] = render_to_string(:partial => 'created')
+    #   #redirect_back_or_default('/')
+    #   #send_account_state_notification @user
+    #   super
+    # else
+    #   if @user.linked_to_twitter? || @user.linked_to_facebook?
+    #     render :action => 'new_via_third_party'
+    #   else
+    #     render :action => 'new'
+    #   end
+    # end
+
+    super
   end
 
 protected
